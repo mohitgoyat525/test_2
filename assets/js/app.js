7fa232229c5fb3ef7d635ee28507e6f39db2513c
mohitgoyat525 <montyits71@gmail.com>
2024-10-07 10:37:15 +0530

    add-changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,14 +12,14 @@ accordionItems.forEach((item) => {
     if (accordionActive && accordionActive !== item) {
       accordionActive.classList.remove("active");
       accordionActive.querySelector(".accordionInfo").classList.add("hidden");
-      accordionActive.querySelector("img").classList.remove("rotate-[360deg]");
-      accordionActive.querySelector("img").classList.remove("duration-[2s]");
+      accordionActive.querySelector("img").classList.remove("rotate-[180deg]");
+      accordionActive.querySelector("img").classList.remove("duration-[0.5s]");
       accordionActive.querySelector("img").src = "./assets/images/svg/plus-icon.svg";
     }
     item.classList.toggle("active");
     content.classList.toggle("hidden");
-    icon.classList.toggle("rotate-[360deg]");
-    icon.classList.toggle("duration-[2s]");
+    icon.classList.toggle("rotate-[180deg]");
+    icon.classList.toggle("duration-[0.5s]");
     icon.src = item.classList.contains("active")
       ? "./assets/images/svg/subtract-icon.svg"
       : "./assets/images/svg/plus-icon.svg";
